a91084ff51851c99e80c7c6bb2b542ba6845a037
Dubesor22 <dubesor22@gmail.com>
2022-04-28 20:05:06 +0200

    Boton Borrado

--- main.js
+++ main.js
@@ -5,11 +5,11 @@ const passConfirm = document.querySelector("#password2");
 const boton = document.querySelector("#btn");
 let msg = document.querySelector("#msg");
 let validationEmail = /(\w+?@\w+?\x2E.+)/;
+let borrado = document.querySelector("#delete");
 let usuarios = [];
 
 //funcion de validar los campos
 function validar() {
-
   if (
     nombre.value === "" ||
     email.value === "" ||
@@ -30,41 +30,45 @@ function validar() {
 }
 
 //funcion para subir al LocalStorage
-function enviarDatos(){
-
-  if(validationEmail.test(email.value) == true){
+function enviarDatos() {
+  if (validationEmail.test(email.value) == true) {
     let info = {
       nombre: nombre.value,
       correo: email.value,
-      contraseña: password.value
-    }
+      contraseña: password.value,
+    };
     usuarios.push(info);
-    localStorage.setItem('Info', JSON.stringify(usuarios));
-  } 
+    localStorage.setItem("Info", JSON.stringify(usuarios));
+  }
 }
 
 //funcion para comparar contraseñas
-function compararContraseña(e){
+function compararContraseña(e) {
   e.preventDefault();
-  if(password.value === passConfirm.value){
+  if (password.value === passConfirm.value) {
     validar();
     enviarDatos();
-    nombre.value = '';
-    email.value = '';
-    password.value = '';
-    passConfirm.value = '';
-  }else{
-    password.value = '';
-    passConfirm.value = '';
-    msg.innerHTML = 'Las contraseñas no son iguales.'
+    nombre.value = "";
+    email.value = "";
+    password.value = "";
+    passConfirm.value = "";
+  } else {
+    password.value = "";
+    passConfirm.value = "";
+    msg.innerHTML = "Las contraseñas no son iguales.";
     setTimeout(function () {
       msg.innerHTML = "";
     }, 5000);
   }
 }
 
-boton.addEventListener('click', compararContraseña)
+boton.addEventListener("click", compararContraseña);
 
+//Funcion de borrar toda la memoria y pantalla (reset)
+function borrar() {
+  localStorage.clear();
+  console.clear();
+  listarUsuario.remove();
+}
 
-
-
+borrado.addEventListener("click", borrar);
