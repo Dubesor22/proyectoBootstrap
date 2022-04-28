5c079b9ea97e5ce6730b2dc3d3891622044d2c1c
Dubesor22 <dubesor22@gmail.com>
2022-04-28 16:09:12 +0200

    validacionJS

---
+++ main.js
@@ -0,0 +1,34 @@
+const nombre = document.querySelector("#name");
+const email = document.querySelector("#mail");
+const password = document.querySelector("#password");
+const passConfirm = document.querySelector("#password2");
+const boton = document.querySelector("#btn");
+let msg = document.querySelector("#msg");
+let validationEmail = /(\w+?@\w+?\x2E.+)/;
+let usuarios = [];
+
+//funcion de validar los campos
+function validar(e) {
+  e.preventDefault();
+
+  if (
+    nombre.value === "" ||
+    email.value === "" ||
+    password.value === "" ||
+    passConfirm.value === ""
+  ) {
+    msg.innerHTML = "Please enter all fields";
+  } else if (validationEmail.test(email.value) !== true) {
+    msg.innerHTML = "Please enter a correct email";
+  } else {
+    msg.innerHTML = "Success";
+  }
+
+  setTimeout(function () {
+    msg.innerHTML = "";
+  }, 3000);
+}
+
+function enviaDatos() {}
+
+boton.addEventListener("click", validar);
